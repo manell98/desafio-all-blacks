e69ab247a5e2f2cb89ff62caf41f3019c6beb103
Emanuell Santos <dfmanu06@gmail.com>
2019-08-09 14:20:35 -0300

    Models + AdminLTE

---
+++ app/Models/Torcedores.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Torcedores extends Model
+{
+    protected $table = 'torcedores';
+    public $timestamps = false;
+}
---
+++ database/migrations/2019_08_09_160912_create_torcedores_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTorcedoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('torcedores', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome', 256);
+            $table->string('documento', 14);
+            $table->string('cep', 9);
+            $table->string('endereco', 256);
+            $table->string('bairro', 256);
+            $table->string('cidade', 45);
+            $table->string('uf', 2);
+            $table->string('telefone', 14);
+            $table->string('email')->unique();
+            $table->enum('ativo',['1', ''])->default('1');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('torcedores');
+    }
+}
