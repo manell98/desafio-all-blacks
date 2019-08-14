8e20b1d2389d24f26c1aaa6820ac2993effca46b
Emanuell Santos <dfmanu06@gmail.com>
2019-08-14 18:56:09 -0300

    Organizando rotas

--- routes/web.php
+++ routes/web.php
@@ -6,21 +6,17 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
     Route::any('/xml/cadastra/clientes-xml/{email}', 'Admin\TorcedorController@cadastraClienteXml')->name('torcedores.cadastraClienteXml');
     Route::any('/xml/form-xml', 'Admin\TorcedorController@formXml')->name('torcedores.formXml');
     Route::any('/xml/upload-xml', 'Admin\TorcedorController@uploadXml')->name('torcedores.uploadXml');
+    
     Route::any('/torcedores/excel', 'Admin\TorcedorController@export')->name('torcedores.excel');
+
     Route::resource('torcedores', 'Admin\TorcedorController');
+    
     Route::get('/', 'Admin\HomeController@index')->name('admin');
 
 });
 
 Route::get('/', 'Auth\LoginController@login' );
-/******************************************************************
- * Rotas de Autenticação
- ******************************************************************/
-// Authentication Routes...
+
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login');
 Route::post('logout', 'Auth\LoginController@logout')->name('logout');
-/******************************************************************
- * Rotas de Autenticação
- ******************************************************************/
-
