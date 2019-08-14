c31523e706fcb51a802a805cfbfdfa953c11d711
Emanuell Santos <dfmanu06@gmail.com>
2019-08-14 18:44:44 -0300

    Logica de exportar pra Excel

---
+++ app/Exports/TorcedoresExport.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\FromQuery;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+use App\Models\Torcedores;
+use DB;
+
+class TorcedoresExport implements FromQuery, WithMapping, WithHeadings, WithEvents
+{ 
+    use Exportable;
+
+    public function registerEvents(): array
+    {
+        $styleArray = [
+            'font' => [
+                'bold' => true,
+            ]
+        ];
+
+        return [
+            AfterSheet::class => function(AfterSheet $event) use ($styleArray) {
+                $event->sheet->getStyle('A1:I1')->applyFromArray($styleArray);
+            },
+        ];    
+    }
+
+    public function query()
+    {
+        return DB::table('torcedores')->where('ativo', '1')->orderBy('nome', 'asc');
+    }
+
+    public function map($torcedores): array
+    {
+        return [
+            $torcedores->nome,
+            $torcedores->documento,
+            $torcedores->cep,
+            $torcedores->endereco,
+            $torcedores->bairro,
+            $torcedores->cidade,
+            $torcedores->uf,
+            $torcedores->telefone,
+            $torcedores->email,
+        ];
+    }
+    
+    public function headings(): array
+    {
+        return [
+            'Nome',
+            'Documento',
+            'Cep',
+            'Endereço',
+            'Bairro',
+            'Cidade',
+            'Estado',
+            'Telefone',
+            'E-mail',
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
     Route::any('/xml/cadastra/clientes-xml/{email}', 'Admin\TorcedorController@cadastraClienteXml')->name('torcedores.cadastraClienteXml');
     Route::any('/xml/form-xml', 'Admin\TorcedorController@formXml')->name('torcedores.formXml');
     Route::any('/xml/upload-xml', 'Admin\TorcedorController@uploadXml')->name('torcedores.uploadXml');
+    Route::any('/torcedores/excel', 'Admin\TorcedorController@export')->name('torcedores.excel');
     Route::resource('torcedores', 'Admin\TorcedorController');
     Route::get('/', 'Admin\HomeController@index')->name('admin');
 
